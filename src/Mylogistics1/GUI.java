f1e3d04721e494503ceda0b8fb6a6e1bb2562f5b
Heanzy <289139701@qq.com>
2018-05-05 21:48:11 +0800

    增加城市间物流的中转功能

--- src/Mylogistics1/GUI.java
+++ src/Mylogistics1/GUI.java
@@ -1,12 +1,12 @@
 package Mylogistics1;
-
 import JSHOP2.*;
 
 import java.util.*;
 
-public class GUI{
+public class gui{
     public static void main(String[] args) {
         problem.getPlans();
         new JSHOP2GUI();
     }
 }
+
